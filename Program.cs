de6f894137fed684a8536d73c1d81cee5da3ca43
lneiz <yuntsevich@ymail.com>
2023-02-04 02:35:47 +0500

    task 10 - done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int SecDigitOfNum(int number)
+{
+    return (number % 100) / 10;
+}
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Вторая цифра числа: " + SecDigitOfNum(num));
