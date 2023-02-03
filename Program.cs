9f505757036c373f6ea415de3c34ca93f605d5da
lneiz <yuntsevich@ymail.com>
2023-02-04 03:38:15 +0500

    task 15 - done

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ Console.Write("Вторая цифра числа: " + SecDigitOfNum(num));
 */
 
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа.
-
+/*
 int ThirdDigitOfNum(int number)
 {
     int result = 0;
@@ -29,3 +29,23 @@ Console.Write("Введите любое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Третья цифра числа: " + ThirdDigitOfNum(num));
+*/
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+bool HolidayDetect(int dayNum)
+{
+    if((dayNum == 6) | (dayNum == 6)) return true;
+    else return false;
+}
+
+string TranslateToRus(bool vacation)
+{
+    if(vacation == true) return "выходной";
+    else return "не выходной";
+}
+
+Console.Write("Введите номер дня недели (1-7): ");
+int numOfDay = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Безусловно, это " + TranslateToRus(HolidayDetect(numOfDay)));
