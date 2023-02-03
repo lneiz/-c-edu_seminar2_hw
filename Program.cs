7ee84d4b3ca250492eb3e7fc466830619d2bdb1b
lneiz <yuntsevich@ymail.com>
2023-02-04 03:12:21 +0500

    task 13 - done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-
+/*
 int SecDigitOfNum(int number)
 {
     return (number % 100) / 10;
@@ -9,3 +9,23 @@ Console.Write("Введите трехзначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Вторая цифра числа: " + SecDigitOfNum(num));
+*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа.
+
+int ThirdDigitOfNum(int number)
+{
+    int result = 0;
+    if((number < 100) && (-100 < number)) result = -1;
+    else
+    {
+        while(number > 999) number = number / 10;
+        result = number %10;
+    }
+    return result;
+}
+
+Console.Write("Введите любое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Третья цифра числа: " + ThirdDigitOfNum(num));
